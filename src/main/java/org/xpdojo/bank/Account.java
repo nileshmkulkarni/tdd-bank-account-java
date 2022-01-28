08dceac487c159aeef1f75b3fbd2e012e79842e8
nileshmkulkarni <nileshmkulkarni@gmail.com>
2022-01-28 18:43:54 +0530

    initialAccount_shouldHaveZeroBalance

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,19 @@
 package org.xpdojo.bank;
 
 public class Account {
+
+    public static Account emptyAcccount(){
+        return new Account();
+    }
+    private Account(){
+        balance=0;
+    }
+
+    public int balanc(){
+        return balance;
+    }
+
+    public void deposit(int amount){
+        blance +=amount;
+    }
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -1,15 +1,32 @@
 package org.xpdojo.bank;
-
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Disabled;
+import static org.xpdojo.bank.Account.emptyAccount;
+import static org.assertj.core.api.Assertions
+
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AccountTest {
 
     @Test
-	@Disabled
-    public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+	    public void initialAccount_shouldHaveZeroBalance() {
+        Acccount account = new emptyAccount();
+        assertThat(emptyAccount.balance()).isEqualTo(0);
+    }
+
+    @Test
+    public void depositAnAmount_shouldIncreaeBalanceBySameAmount(){
+        Acccount account = new emptyAccount();
+        account.deposit(amount 10);
+        assertThat(emptyAccount.balance()).isEqualTo(10)
+    }
+
+    @Test
+    public void depositAnAmount_shouldIncreaeBalanceByBothAmount(){
+        Acccount account = new emptyAccount();
+        account.deposit(amount 10);
+        account.deposit(amount 20);
+        assertThat(emptyAccount.balance()).isEqualTo(30)
     }
 }
